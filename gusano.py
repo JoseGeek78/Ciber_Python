ed214af72bf411f29bdd15aeaabe47173b63b9fc
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-09 07:52:31 +0100

    Updated

--- gusano.py
+++ gusano.py
@@ -5,4 +5,6 @@ import sys
 
 if len(sys.argv) == 2:
     for num in range(0, int(sys.argv[1])):
-        shutil.copy()
+        shutil.copy(sys.argv[0], sys.argv[0] + f'{num}.py')
+else:
+    print('Envía dos parámetros')
