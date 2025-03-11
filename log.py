6f0c8d59b9fcc5f43497c80855f6052db5f4b40a
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-11 16:16:44 +0100

    .

---
+++ log.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from scapy.all import sniff, IP, TCP
+import time
+from threading import Timer
+
+# Diccionario para almacenar los datos de SYN por IP: {ip: (contador, primer_timestamp)}
