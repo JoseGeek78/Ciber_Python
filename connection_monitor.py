6dd03acbf97780b559208c9f578aa61c6e787df4
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-17 08:25:49 +0100

    Updated

--- connection_monitor.py
+++ connection_monitor.py
@@ -5,4 +5,6 @@ SAFE_PORTS = {22, 80, 443, 53}
 
 # Iterar sobre las conexiones activas
 for conn in psutil.net_connections(kind='inet'):
+    laddr, raddr, status = conn.laddr, conn.raddr, conn.status
     
+    # Si hay una dirección remota y el puerto no está en la lista segura
