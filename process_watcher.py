8518475f568d77f2ecfaa46f025f2beba6038c4a
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-20 08:42:56 +0100

    Updated

--- process_watcher.py
+++ process_watcher.py
@@ -2,3 +2,6 @@ import psutil
 
 # Lista de procesos maliciosos conocidos
 MALICIOUS_PROCESSES = {"malware.exe", "keylogger.py", "ransomware"}
+
+for proc in psutil.process_iter(attrs=['pid', 'name']):
+    
