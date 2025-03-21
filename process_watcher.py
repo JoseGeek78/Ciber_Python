6c49051350ce995c6cae2d8700ac8775b80ab92c
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-21 08:37:18 +0100

    Updated

--- process_watcher.py
+++ process_watcher.py
@@ -5,4 +5,4 @@ MALICIOUS_PROCESSES = {"malware.exe", "keylogger.py", "ransomware"}
 
 for proc in psutil.process_iter(attrs=['pid', 'name']):
     if proc.info['name'] in MALICIOUS_PROCESSES:
-        
+        print(f"[ALERTA] Proceso malicioso detectado: {proc.info['name']} (PID: {proc.info['pid']})")
