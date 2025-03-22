d035130eb8ab8f82ec48d7a4040c84981d89e690
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-22 08:31:24 +0100

    Updated

---
+++ block_malicious_ip.py
@@ -0,0 +1,7 @@
+import os
+
+MALICIOUS_IP = "111.111.1.111" # IP maliciosa a bloquear
+
+# Ejecutar comando iptables para bloquear la IP
+os.system(f"sudo iptables -A INPUT -s {MALICIOUS_IP} -j DROP")
+print(f"[+] 1bloqueada la IP maliciosa: {MALICIOUS_IP}")
