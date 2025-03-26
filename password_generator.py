1dfb4f17e63a04cf2c4901be0df22e29da1962b5
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-26 16:51:34 +0100

    Updated

--- password_generator.py
+++ password_generator.py
@@ -1,2 +1,7 @@
 import random
 import string
+
+def generar_password(longitud):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(caracteres) for i in range(longitud))
+    return password
