7aaba69ab81e0fa946f03f5c1f3c04fdff93820e
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-27 15:54:45 +0100

    Updated

--- password_generator.py
+++ password_generator.py
@@ -5,3 +5,6 @@ def generar_password(longitud):
     caracteres = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(caracteres) for i in range(longitud))
     return password
+
+print("Contraseña generada: ", generar_password(12))
+# Esta función genera una contraseña aleatoria de longitud 12 utilizando letras mayúsculas, minúsculas, dígitos y caracteres especiales.
