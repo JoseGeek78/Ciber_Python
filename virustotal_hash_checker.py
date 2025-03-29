9275ca6931550a1ff8bb3439ffb8a769e1677d9f
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-29 08:20:51 +0100

    .

--- virustotal_hash_checker.py
+++ virustotal_hash_checker.py
@@ -1,1 +1,16 @@
 import hashlib
+import requests
+
+API_KEY = "Tu_ API_KEY"  # Replace with your VirusTotal API key
+HASH = "Tu_HASH"  # Replace with the hash you want to check
+URL = "https://www.virustotal.com/vtapi/v2/file/report"
+
+def get_file_report(api_key, hash_value):
+    """Fetch the file report from VirusTotal using the hash value."""
+    params = {
+        'apikey': api_key,
+        'resource': hash_value,
+    }
+    response = requests.get(URL, params=params)
+    return response.json()
+
