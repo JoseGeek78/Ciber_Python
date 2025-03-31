9add27c5c6acbe29be9a08ce34c3cbcd93ce4593
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-03-31 09:50:15 +0200

    .

--- auto_shutdown_intrusion.py
+++ auto_shutdown_intrusion.py
@@ -1,4 +1,15 @@
 import os
-import shutil
+import psutilsutil
 
 MALICIOUS_PROCESSV= "hacker_tool.exe"
+
+for proc in psutil.process_iter(attrs={['name']}):
+    if proc.info['name'] == MALICIOUS_PROCESS:
+        print(f"Malicious process {MALICIOUS_PROCESS} detected!")
+        os.system("shutdown /s /t 1")  # Shutdown the system immediately
+        break
+    else:
+        print(f"Process {proc.info['name']} is safe.")
+        continue
+# This script checks for a specific malicious process and shuts down the system if found.
+    
