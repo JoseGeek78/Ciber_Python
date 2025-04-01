ba6412748eb8f8f4d34f09268679788eca47185b
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-04-01 09:29:06 +0200

    .

--- auto_shutdown_intrusion.py
+++ auto_shutdown_intrusion.py
@@ -1,10 +1,3 @@
 import os
 import psutil
 
-MALICIOUS_PROCESS= "hacker_tool.exe"
-
-for proc in psutil.process_iter(attrs={['name']}):
-    if proc.info['name'] == MALICIOUS_PROCESS:
-        print("[ALERTA] Proceso malicioso detectado. Apagando el sistema...")
-        os.system("shutdown /s /t 1")  # Para Windows
-        # os.system("sudo shutdown -h now")  # Para Linux
