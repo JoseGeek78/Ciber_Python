c4cfa784b157cd30aad73f1599d445cb2c98f5e8
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-04-02 10:26:15 +0200

    Updated

--- file_access_logger.py
+++ file_access_logger.py
@@ -1,7 +1,15 @@
 import os
 import time
 
-LOG_FILE ="/var/log/auth.log"
+LOG_FILE = "/var/log/auth.log"
 
 def monitor_logs():
-    
+    with open(LOG_FILE, "r") as f:
+        f.seek(0, os.SEEK_END)
+        while True:
+            line = f.readline()
+            if line and "/etc/passwd" in line:
+                print(f"[ALERTA] Acceso sospechoso detectado: {line.strip()}")
+            time.sleep(1)
+
+monitor_logs()
