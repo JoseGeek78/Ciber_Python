eeb9bf64135e55d04b3d80f2cc0651d310400ea5
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-04-03 09:42:06 +0200

    Updated

---
+++ suspicious_screenshot.py
@@ -0,0 +1,14 @@
+import pyautogui
+import psutil
+
+MALICIOUS_PROCESSES = "snifre.exe, sniffer.exe, sniffer, snif.exe, snif.exe, sniff.exe, sniff, sniffler.exe, sniffler"
+
+for proc in psutil.process_iter(['pid', 'name']):
+    if proc.info['name'] in MALICIOUS_PROCESSES.split(", "):
+        print(f"Suspicious process detected: {proc.info['name']} (PID: {proc.info['pid']})")
+        screenshot = pyautogui.screenshot()
+        screenshot.save(f"suspicious_process_{proc.info['pid']}.png")
+        print(f"Screenshot saved as suspicious_process_{proc.info['pid']}.png")
+        break
+else:
+    print("No suspicious processes found.")
