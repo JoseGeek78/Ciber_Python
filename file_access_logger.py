ec9ab2845f5207cde482e99ed8404363fe100608
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-04-12 08:17:53 +0200

    .

--- file_access_logger.py
+++ file_access_logger.py
@@ -1,15 +1,7 @@
 import os
 import time
+import logging
 
-LOG_FILE = "/var/log/auth.log"
-
-def monitor_logs():
-    with open(LOG_FILE, "r") as f:
-        f.seek(0, os.SEEK_END)
-        while True:
-            line = f.readline()
-            if line and "/etc/passwd" in line:
-                print(f"[ALERTA] Acceso sospechoso detectado: {line.strip()}")
-            time.sleep(1)
-
-monitor_logs()
+# Configuración del nombre del archivo de log a monitorear.
+LOG_FILE = "var/log/auth.log"
+# Palabra clave o patrón que se quiere detectar en el archivo. En este caso, indicamos "/etc/passwd".
