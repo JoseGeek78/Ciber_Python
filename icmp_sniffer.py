d1cfc30288812f8b145c4d33814a6c5103fd4641
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-04-17 16:17:46 +0200

    .

--- icmp_sniffer.py
+++
@@ -1,6 +0,0 @@
-from scapy.all import sniff, IP, ICMP # Importamos las funciones necesarias de la librería Scapy
-#
-# Configuración del nombre del archivo de log a monitorear. 
-# LOG_FILE = "/var/log/auth.log"
-# # Palabra clave o patrón que se quiere detectar en el archivo. En este caso, indicamos "/etc/passwd".     
-# ALERT_KEYWORD = "/etc/passwd"
