60a6ed8978315946ce88e1cef064d342b3fd40f0
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-04-17 16:14:16 +0200

    .

--- icmp_sniffer.py
+++ icmp_sniffer.py
@@ -1,8 +1,2 @@
 from scapy.all import sniff, IP, ICMP # Importamos las funciones necesarias de la librería Scapy
 
-# Definimos una función que será ejecutada cada vez que se capture un paquete
-def detectar_ping(packet):
-    if packet.haslayer(ICMP): # Verificamos si el paquete tiene la capa ICMP (indicador de un ping)
-        print(f"[ALERTA] Ping detectado desde {packet[IP].src}") # Mostramos una alerta con la IP de origen
-        
-sniff(filter="icmp", prn=detectar_ping, store=False)
